4783e4e6e3ee2c82e5fb163b5ec83dbe90a7bdf1
Rob Bettison <rbettison>
2023-10-09 16:45:44 +0200

    escaping apostrophe

--- app/blog/[blogId]/not-found.tsx
+++ app/blog/[blogId]/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <main className={styles.container}>
             <h1>There was a problem.</h1>
-            <p>We couldn't find that blog post.</p>
+            <p>We couldn&apos;t find that blog post.</p>
             <p>Go back to <Link href="/blog"><span>all blogs</span></Link>.</p>
         </main>
     );

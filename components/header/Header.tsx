73557df88e1f0e7dfa019bcce0502d1ec9b56b01
Rob Bettison <rbettison>
2023-10-24 22:26:38 +0200

    Header nav decoration

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,9 +2,9 @@
 import '../../app/globals.css'
 import styles from './header.module.css';
 import Link from 'next/link';
-import { MouseEvent, useContext, useEffect } from 'react';
+import { MouseEvent, useContext, useEffect, useState } from 'react';
 import { ThemeContext, ThemeContextType } from '@/contexts/ThemeContext';
-import Switch from '@mui/material/Switch';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
 
@@ -23,6 +23,8 @@ export default function Header() {
       console.log('setting the theme in local storage: ' + theme);
     }, [theme])
 
+    const pathname = usePathname();
+
     return (
 
         <div className="col-span-2 md:text-right md:fixed md:left-16 p-4" id="navbar"> 
@@ -39,19 +41,19 @@ export default function Header() {
         </div>
       
         <ul className={`md:flex flex-col gap-4 text-md font-bold text-right md:text-left hidden fixed md:left-auto md:top-auto md:static right-4 top-12 p-4 md:p-0 z-40 ${theme === "light" ? "bg-gray-300" : "bg-purple-700"} md:bg-inherit`} id="navbar">
-            <li className="hover:text-highlighttext">
+            <li className={`hover:text-highlighttext ${pathname === '/' ? 'border-l-4 border-current pl-2':""}`}>
                 <Link href='/' onClick={(e) => closeMenu(e)}>home</Link>
             </li>
-            <li className="hover:text-highlighttext">
+            <li className={`hover:text-highlighttext ${pathname === '/contact' ? 'border-l-4 border-current pl-2':""}`}>
                 <Link href='/contact' onClick={(e) => closeMenu(e)}>contact</Link>
             </li>
-            <li className="hover:text-highlighttext">
+            <li className={`hover:text-highlighttext ${pathname === '/services' ? 'border-l-4 border-current pl-2':""}`}>
                 <Link href='/services' onClick={(e) => closeMenu(e)}>services</Link>
             </li>
-            <li className="hover:text-highlighttext">
+            <li className={`hover:text-highlighttext ${pathname === '/portfolio' ? 'border-l-4 border-current pl-2':""}`}>
                 <Link href='/portfolio' onClick={(e) => closeMenu(e)}>portfolio (coming soon)</Link>
             </li>
-            <li className="hover:text-highlighttext">
+            <li className={`hover:text-highlighttext ${pathname?.includes('/blog') ? 'border-l-4 border-current pl-2':""}`}>
                 <Link href='/blog' onClick={(e) => closeMenu(e)}>blog</Link>
             </li>
             <li className='cursor-pointer'>

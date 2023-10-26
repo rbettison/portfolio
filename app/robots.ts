21a62c0acbd8befb3c6570a419924318af1e4a9f
Rob Bettison <rbettison>
2023-10-26 11:12:48 +0200

    debugging robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             [
                 {
                     userAgent: "Twitterbot",
-                    allow: "/" 
+                    disallow: "" 
                 }
             ]
     }

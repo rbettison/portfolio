43d6d73d25d288c3b272935d815d778ca509a903
Rob Bettison <rbettison>
2023-11-23 22:35:09 +0100

    Adding twitter and medium socials

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -9,7 +9,15 @@ export default function Hero() {
 
     return (
 
-            <div className="h-screen grid md:grid-cols-5 md:grid-rows-5 grid-cols-3 grid-rows-2">
+            <div className="h-screen grid md:grid-cols-5 md:grid-rows-5 grid-cols-3 grid-rows-2 relative">
+                <div className='md:row-start-1 md:col-start-2 md:col-span-2 flex flex-row absolute top-8 md:top-auto md:static'>
+                    <Link href="https://twitter.com/robbettison" target="_blank" className='w-auto h-full flex items-end'>
+                        <img className="h-12" src="/socials/twitter.png" />
+                    </Link>
+                    <Link href="https://medium.com/@rob.bettison94" target="_blank" className='w-auto h-full flex items-end'>
+                        <img className="h-12" src='/socials/Medium-Symbol-Black-RGB@1x.png'/>
+                    </Link>
+                </div>
                 <p className="md:row-start-2 md:col-start-2 md:col-span-3 text-4xl row-start-1 col-span-3 self-end">
                     Hi, I&apos;m Rob. I&apos;m a software 
                     engineer and 
